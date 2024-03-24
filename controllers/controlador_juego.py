f1b4310088bc781ba3beffbec2b9b66b4fe75a66
Juan David <juan.loaiza.santiago@correounivalle.edu.co>
2024-03-23 22:06:05 -0500

    refactorization controlador-modelo juego

--- controllers/controlador_juego.py
+++ controllers/controlador_juego.py
@@ -1,12 +1,15 @@
 # controlador_principal.py
 
 import os
-from PyQt5 import QtWidgets, QtCore
-from PyQt5.QtGui import QStandardItemModel, QPixmap
+from PyQt5.QtGui import QPixmap, QIcon
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import Qt, QSize
+from PyQt5.QtGui import QStandardItemModel, QPixmap, QIcon
 from views.vista_juego import Ui_MainWindow
 from models.modelo_juego import modelo_juego
 from views.sm_dialog_clean import Ui_Dialog as sm_dialog_clean
 from models.tools.temporizador import Temporizador
+from models.tools.dialog import Dialog
 
 
 class controlador_juego:
@@ -43,11 +46,43 @@ class controlador_juego:
 
         self.modelo.setTabla(self.ui.table_mapa)
 
+    def actualizar_tabla(self, ambiente, coordenadas_agente, coordenadas_meta):
+        for i, fila in enumerate(ambiente):
+            for j, elemento in enumerate(fila):
+
+                item = QtWidgets.QTableWidgetItem()
+                icon = None
+                pixmap = None
+
+                if [i, j] == coordenadas_agente:
+                    pixmap = QPixmap(self.modelo.mando)
+                elif [i, j] == coordenadas_meta:
+                    pixmap = QPixmap(self.modelo.grogu)
+                else:
+                    if elemento == self.modelo.env_objects_dic['vacio']:
+                        pixmap = QPixmap(self.modelo.vacio)
+                    elif elemento == self.modelo.env_objects_dic['pared']:
+                        pixmap = QPixmap(self.modelo.pared)
+                    elif elemento == self.modelo.env_objects_dic['enemigo']:
+                        pixmap = QPixmap(self.modelo.enemigo)
+                    elif elemento == self.modelo.env_objects_dic['nave']:
+                        pixmap = QPixmap(self.modelo.nave)
+                    else:
+                        if self.debug:
+                            print(
+                                "modelo_juego.py: actualizar_tabla ha omitido cargar el elemento ", elemento)
+
+                icon = QIcon(pixmap)
+                item.setData(Qt.DecorationRole, icon)
+                icon_size = 45
+                self.ui.table_mapa.setIconSize(QSize(icon_size, icon_size))
+                self.ui.table_mapa.setItem(i, j, item)
+
     def mostrar(self, main_window):
         self.cargar(main_window)
         self.MainWindow.show()
         Temporizador.iniciar(0.1)
-        self.modelo.iniciar_juego()
+        self.iniciar_juego()
 
     def block_focus(self, window):
         self.MainWindow.setEnabled(False)
@@ -57,3 +92,15 @@ class controlador_juego:
         self.MainWindow.setMinimumSize(self.MainWindow.minimumSizeHint())
         self.MainWindow.setMaximumSize(16777215, 16777215)
         self.MainWindow.setEnabled(True)
+
+    def iniciar_juego(self):
+        self.modelo.iniciar_juego()
+        self.animar_juego()
+
+    def animar_juego(self):
+        for paso in self.modelo.camino:
+            Temporizador.iniciar(1.5)
+            self.actualizar_tabla(
+                self.modelo.ambiente, paso, self.modelo.estado_objetivo.get_coordenadas())
+
+        Dialog.mostrar(self.modelo.resultado)
--- models/modelo_juego.py
+++ models/modelo_juego.py
@@ -24,6 +24,10 @@ class modelo_juego:
     def __init__(self):
         self.tabla_juego = None
         self.debug = False
+        self.camino = None
+        self.ambiente = None
+        self.estado_objetivo = None
+        self.resultado = "Estoy perdido"
 
         self.assets_dic = wtools.reconocer_assets()
         self.env_objects_dic = wtools.reconocer_objetos()
@@ -39,86 +43,50 @@ class modelo_juego:
     def setTabla(self, tabla,):
         self.tabla_juego = tabla
 
-    def actualizar_tabla(self, ambiente, coordenadas_agente, coordenadas_meta):
-        for i, fila in enumerate(ambiente):
-            for j, elemento in enumerate(fila):
-
-                item = QtWidgets.QTableWidgetItem()
-                icon = None
-                pixmap = None
-
-                if [i, j] == coordenadas_agente:
-                    pixmap = QPixmap(self.mando)
-                elif [i, j] == coordenadas_meta:
-                    pixmap = QPixmap(self.grogu)
-                else:
-                    if elemento == self.env_objects_dic['vacio']:
-                        pixmap = QPixmap(self.vacio)
-                    elif elemento == self.env_objects_dic['pared']:
-                        pixmap = QPixmap(self.pared)
-                    elif elemento == self.env_objects_dic['enemigo']:
-                        pixmap = QPixmap(self.enemigo)
-                    elif elemento == self.env_objects_dic['nave']:
-                        pixmap = QPixmap(self.nave)
-                    else:
-                        if self.debug:
-                            print(
-                                "modelo_juego.py: actualizar_tabla ha omitido cargar el elemento ", elemento)
-
-                icon = QIcon(pixmap)
-                item.setData(Qt.DecorationRole, icon)
-                icon_size = 45
-                self.tabla_juego.setIconSize(QSize(icon_size, icon_size))
-                self.tabla_juego.setItem(i, j, item)
-
     def iniciar_juego(self):
-
         # Cargar archivo de mundo
         file_selector = File_selector()
         archivo = file_selector.select("data/worlds")
 
         # Generar mundo
-        ambiente = wtools.generar_mundo(archivo)
+        self.ambiente = wtools.generar_mundo(archivo)
 
         # Inicializar estado inicial y estado objetivo
         x_ini, y_ini = wtools.determinar_posicion(
-            ambiente, self.env_objects_dic['agente'])
+            self.ambiente, self.env_objects_dic['agente'])
         x_meta, y_meta = wtools.determinar_posicion(
-            ambiente, self.env_objects_dic['meta'])
+            self.ambiente, self.env_objects_dic['meta'])
 
         mando = Agente([x_ini, y_ini])
         estado_inicial = Estado(mando.get_x(), mando.get_y())
-        estado_objetivo = Estado(x_meta, y_meta)
+        self.estado_objetivo = Estado(x_meta, y_meta)
 
         # Eliminar el estado_inicial del ambiente
         print_debug("x_ini: {}, y_ini: {}".format(str(x_ini), str(y_ini)))
-        ambiente[x_ini][y_ini] = "0"
+        self.ambiente[x_ini][y_ini] = "0"
 
         estado_actual = estado_inicial
         nuevo_estado = None
-        resultado = "Estoy perdido"
-
-        self.actualizar_tabla(
-            ambiente, mando.get_coordenadas(), estado_objetivo.get_coordenadas())
 
         while True:
-            problema = Problema(estado_actual, estado_objetivo, ambiente)
+            problema = Problema(
+                estado_actual, self.estado_objetivo, self.ambiente)
             print_debug("Coordenadas estado_actual: {}".format(
                 str(estado_actual.get_coordenadas())))
             if (problema.es_objetivo(estado_actual)):
-                resultado = "Lo logré"
+                self.resultado = "Lo logré"
                 break
 
             acciones_validas = problema.generar_acciones(estado_actual)
             if len(acciones_validas) == 0:
-                resultado = "Estoy perdido"
+                self.resultado = "Estoy perdido"
                 break
             print_debug("Acciones válidas desde el estado actual: {}".format(str([
                 accion.nombre for accion in acciones_validas])))
 
             accion = mando.tomar_decision(acciones_validas)
             if accion == None:
-                resultado = "No se que hacer en esta situacion :("
+                self.resultado = "No se que hacer en esta situacion :("
                 break
             print_debug("El agente decidió: {}".format(
                 str(accion.get_nombre())))
@@ -131,11 +99,4 @@ class modelo_juego:
             estado_actual = nuevo_estado
             nuevo_estado = None
 
-        camino = mando.get_pasos()
-
-        for paso in camino:
-            Temporizador.iniciar(1.5)
-            self.actualizar_tabla(
-                ambiente, paso, estado_objetivo.get_coordenadas())
-
-        Dialog.mostrar(resultado)
+        self.camino = mando.get_pasos()

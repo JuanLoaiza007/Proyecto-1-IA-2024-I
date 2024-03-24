5269d86fd17c184c917b313441a2f6f0f19cb5e3
Juan David <juan.loaiza.santiago@correounivalle.edu.co>
2024-03-23 22:27:39 -0500

    refactorization estructura_datos-nodo

--- models/estructura_datos.py
+++ models/estructura_datos.py
@@ -159,3 +159,78 @@ class Problema:
             return nuevo_estado
         else:
             return None
+
+
+class Nodo:
+    """
+    Clase que representa un nodo en un árbol de búsqueda.
+
+    Cada nodo del árbol guarda la siguiente información:
+    - El estado del problema
+    - Una referencia al nodo padre
+    - El operador que se aplicó para generar el nodo
+    - Profundidad del nodo
+    - El costo de la ruta desde la raíz hasta el nodo
+    """
+
+    def __init__(self):
+        """
+        Inicializa un nuevo nodo.
+
+        Atributos:
+        - estado: El estado del problema.
+        - padre: Una referencia al nodo padre.
+        - operador: El operador que se aplicó para generar el nodo.
+        - profundidad: La profundidad del nodo en el árbol.
+        - costo_acumulado: El costo acumulado de la ruta desde la raíz hasta el nodo.
+        """
+        self.estado = None
+        self.padre = None
+        self.operador = None
+        self.profundidad = None
+        self.costo_acumulado = None
+
+    def get_estado(self):
+        return self.estado
+
+    def set_estado(self, estado):
+        self.estado = estado
+
+    def get_padre(self):
+        return self.padre
+
+    def set_padre(self, padre):
+        self.padre = padre
+
+    def get_operador(self):
+        return self.operador
+
+    def set_operador(self, operador):
+        self.operador = operador
+
+    def get_profundidad(self):
+        return self.profundidad
+
+    def set_profundidad(self, profundidad):
+        self.profundidad = profundidad
+
+    def get_costo_acumulado(self):
+        return self.costo_acumulado
+
+    def set_costo_acumulado(self, costo_acumulado):
+        self.costo_acumulado = costo_acumulado
+
+    def expandir(self):
+
+        hijos = []
+        # algun for para las opciones válidas
+        # hijo = Nodo()
+        # hijo.set_estado(nuevo_estado)
+        # hijo.set_padre(self)
+        # hijo.set_operador(operador)
+        # hijo.set_profundidad(self.get_profundidad() + 1)
+        # hijo.set_costo_acumulado(self.get_costo_acumulado() + 1)
+
+        # hijos.append(hijo)
+
+        return hijos

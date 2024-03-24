c8d537e1139ad27480f5fc0d3861930adf5fc50c
Juan David <juan.loaiza.santiago@correounivalle.edu.co>
2024-03-23 20:59:52 -0500

    Data structure implementation (the rest of the logic has been damaged)

--- models/agente_reflejo_simple.py
+++ models/agente_reflejo_simple.py
@@ -1,64 +1,34 @@
-from models.agente import agente
+# [Agente_reflejo_simple.py]
+
+debug = False
 
 
-class agente_reflejo_simple(agente):
-    def __init__(self):
-        super().__init__()
+class agente_reflejo_simple():
+    def __init__(self, coordenadas):
+        self.coordenadas = coordenadas
+        self.pasos = []
 
-        self.tabla_percepcion_accion = [
-            #  Esta libre
-            # left    up    right  down     action
-            ((True, True, True, True, False), 'arriba'),
-            ((True, True, True, False, False), 'arriba'),
-            ((True, True, False, True, False), 'arriba'),
-            ((True, True, False, False, False), 'arriba'),
-            ((True, False, True, True, False), 'izquierda'),
-            ((True, False, True, False, False), 'derecha'),
-            ((True, False, False, True, False), 'izquierda'),
-            ((True, False, False, False, False), 'izquierda'),
-            ((False, True, True, True, False), 'arriba'),
-            ((False, True, True, False, False), 'derecha'),
-            ((False, True, False, True, False), 'abajo'),
-            ((False, True, False, False, False), 'arriba'),
-            ((False, False, True, True, False), 'derecha'),
-            ((False, False, True, False, False), 'derecha'),
-            ((False, False, False, True, False), 'abajo'),
-            ((False, False, False, False, False), 'esperar')
-        ]
+    # Coordenadas del agente
+    def set_coordenadas(self, coordenadas):
+        self.coordenadas = coordenadas
+        self.pasos.append(self.coordenadas)
 
-    def determinar_accion(self, percepcion_actual):
-        if percepcion_actual[-1]:
-            return 'alegrarse'
-        for percepcion, accion in self.tabla_percepcion_accion:
-            if percepcion_actual == percepcion:
+    def get_coordenadas(self):
+        return self.coordenadas
+
+    def get_x(self):
+        return self.coordenadas[0]
+
+    def get_y(self):
+        return self.coordenadas[1]
+
+    def tomar_decision(self, acciones_validas):
+        for accion in acciones_validas:
+            if accion.get_nombre() == "arriba":
                 return accion
-        return 'esperar'
-
-    def tomar_decision(self):
-        percepciones = (
-            self.libre_izquierda(),
-            self.libre_arriba(),
-            self.libre_derecha(),
-            self.libre_abajo(),
-            self.meta_alcanzada()
-        )
-
-        accion = self.determinar_accion(percepciones)
-
-        if accion == 'alegrarse':
-            if self.debug:
-                print("agente_reflejo_simply.py: Estoy en la meta, no me movere")
-            return False
-        if accion == 'esperar':
-            if self.debug:
-                print("agente_reflejo_simply.py: No encuentro que hacer, no me movere")
-            return False
-        else:
-            if self.debug:
-                print("agente_reflejo_simply.py: Me moveré a ", accion)
-            self.set_coordenadas(getattr(self, accion)())
-
-            return True
+            elif accion.get_nombre() == "derecha":
+                return accion
+        return None
 
     def iniciar_viaje(self):
 
@@ -68,15 +38,10 @@ class agente_reflejo_simple(agente):
         while (en_viaje):
             en_viaje = self.tomar_decision()
 
-            for elemento in self.pasos[:-1]:
-                if elemento == self.coordenadas:
-                    if self.debug:
-                        print("agente_reflejo_simply.py: Pensé ir a ",
-                              self.coordenadas, " pero  ya estuve en ", elemento)
-                    resultado = "Ya paśe por aqui, algo anda mal :("
-                    en_viaje = False
-
-        if self.meta_alcanzada():
-            resultado = "Encontré a grogu"
+            # Verificacion de ciclo:
+            # for elemento in self.pasos[:-1]:
+            #     if elemento == self.coordenadas:
+            #         resultado = "Ya paśe por aqui, algo anda mal :("
+            #         en_viaje = False
 
         return self.pasos, resultado
--- models/estructura_datos.py
+++ models/estructura_datos.py
@@ -1,49 +1,166 @@
 # [estructura_datos.py]
 
+from agente_reflejo_simple import agente_reflejo_simple as Agente
+from tools.file_selector import File_selector
+from tools.world_tools import world_tools as wtools
+
+
 class Accion:
     def __init__(self, nombre, dx, dy):
+        """
+        Inicializa una accion
+
+        Args:
+            nombre (str): El nombre de la accion.
+            dx (int): El cambio de coordenada en x.
+            dy (int): El cambio de coordenada en y.
+
+        Returns:
+            None
+        """
         self.nombre = nombre
-        self.dx = dx  # Cambio en y
-        self.dy = dy  # Cambio en x
+        self.dx = dx  # Cambio en x
+        self.dy = dy  # Cambio en y
+
+    def get_nombre(self):
+        return self.nombre
+
+    def get_dx(self):
+        return self.dx
+
+    def get_dy(self):
+        return self.dy
 
 
 class Celda:
     def __init__(self, tipo):
+        """
+        Inicializa una celda
+
+        Args:
+            tipo (str): El tipo de la celda ('pared', 'enemigo', 'nave', 'meta')
+
+        Returns:
+            None
+        """
         self.tipo = tipo
 
 
 class Estado:
-    def __init__(self, x, y):
+    def __init__(self, x: int, y: int):
+        """
+        Inicializa un estado.
+
+        Args:
+            x (int): La coordenada en x.
+            y (int): La coordenada en y.
+
+        Returns:
+            None
+        """
         self.x = x
         self.y = y
 
+    def get_coordenadas(self):
+        return [self.x, self.y]
+
     def __str__(self):
+        """
+        Muestra informacion sobre las coordenadas.
+
+        Args:
+            None
+
+        Returns:
+            Las coordenadas en string.
+        """
         return f"({self.x}, {self.y})"
 
 
 class Problema:
-    def __init__(self, estado_inicial, estado_objetivo, cuadricula):
+    def __init__(self, estado_inicial, estado_objetivo, matriz):
+        """
+        Inicializa un nuevo problema
+
+        Args:
+            estado_inicial (Estado): El estado actual del problema.
+            estado_objetivo (Estado): El estado deseado.
+            matriz (): La matriz que representa el ambiente.
+
+
+        Returns:
+            Las coordenadas en string.
+        """
         self.estado_inicial = estado_inicial
         self.estado_objetivo = estado_objetivo
-        self.cuadricula = cuadricula
+        self.matriz = matriz
 
     def es_valido(self, estado):
+        """
+        Determina si un estado es valido o no.
+        Eg: Un estado NO válido seria que el agente se encontra en las mismas coordenadas que un muro.
+
+        Args:
+            estado (Estado): El estado que se desea evaluar.
+
+        Returns:
+            True, si el estado es válido
+            False, si el estado NO es válido.
+        """
+
         enMatriz = 0 <= estado.x < 10 and 0 <= estado.y < 10
-        enPared = self.cuadricula[estado.x][estado.y] != "pared"
+        if not (enMatriz):
+            return False
+
+        enPared = self.matriz[estado.x][estado.y] != "1"
         return enMatriz and enPared
 
     def es_objetivo(self, estado):
-        return estado in self.estado_objetivo
+        """
+        Determina si un estado es el estado deseado/objetivo.
 
-    def acciones_validas(self, estado):
+        Args:
+            estado (Estado): El estado a evaluar
+
+        Returns:
+            True, si el estado es el objetivo.
+            False, si el estado no es el objetivo
+        """
+        print("(func) es_objetivo: estado: {} objetivo: {}".format(
+            str(estado.get_coordenadas()), str(estado_objetivo.get_coordenadas())))
+        return estado.get_coordenadas() == self.estado_objetivo.get_coordenadas()
+
+    def generar_acciones(self, estado):
+        """
+        Genera las acciones validas para el estado.
+
+        Args:
+            estado (Estado): El estado del que se quiere generar acciones
+
+        Returns:
+            acciones ([Accion, Accion,... Accion]): Un vector de acciones.
+        """
         acciones = []
-        for dx, dy, accion_nombre in [(0, -1, 'arriba'), (0, 1, 'abajo'), (-1, 0, 'izquierda'), (1, 0, 'derecha')]:
+        # OJO: Aquí hay una corrección para los indices de la matriz,
+        # arriba es (0, -1) y así sucesivamente.
+        for dy, dx, accion_nombre in [(0, -1, 'arriba'), (0, 1, 'abajo'), (-1, 0, 'izquierda'), (1, 0, 'derecha')]:
             nuevo_estado = Estado(estado.x + dx, estado.y + dy)
             if self.es_valido(nuevo_estado):
                 acciones.append(Accion(accion_nombre, dx, dy))
         return acciones
 
     def resultado(self, estado, accion):
+        """
+        Genera un nuevo estado aplicando un operador sobre el actual.
+
+        Args:
+            estado (Estado): El estado actual.
+            accion (Accion): La accion que se quiere aplicar al estado.
+
+        Returns:
+            nuevo_estado (Estado): Un nuevo estado que surge de aplicar la accion al estado actual.
+            None si el nuevo estado no cumple con las reglas de juego.
+        """
         nuevo_estado = Estado(estado.x + accion.dx, estado.y + accion.dy)
         if self.es_valido(nuevo_estado):
             return nuevo_estado
@@ -52,4 +169,53 @@ class Problema:
 
 
 if __name__ == '__main__':
-    print("Hola mundo")
+    # Reconocer los objetos del entorno
+    env_objects_dic = wtools.reconocer_objetos()
+
+    # Cargar archivo de mundo
+    file_selector = File_selector()
+    archivo = file_selector.select("data/worlds")
+
+    # Generar mundo
+    ambiente = wtools.generar_mundo(archivo)
+
+    # Inicializar estado inicial y estado objetivo
+    x_ini, y_ini = wtools.determinar_posicion(
+        ambiente, env_objects_dic['agente'])
+    x_meta, y_meta = wtools.determinar_posicion(
+        ambiente, env_objects_dic['meta'])
+
+    mando = Agente([x_ini, y_ini])
+    estado_inicial = Estado(mando.get_x(), mando.get_y())
+    estado_objetivo = Estado(x_meta, y_meta)
+
+    # Eliminar el estado_inicial del ambiente
+    print("x_ini: {}, y_ini: {}".format(str(x_ini), str(y_ini)))
+    ambiente[x_ini][y_ini] = "0"
+
+    estado_actual = estado_inicial
+    nuevo_estado = None
+
+    while True:
+        problema = Problema(estado_actual, estado_objetivo, ambiente)
+        print("Coordenadas estado_actual: {}".format(
+            str(estado_actual.get_coordenadas())))
+        wtools.imprimir_juego(env_objects_dic, ambiente,
+                              [mando.get_x(), mando.get_y()], [x_meta, y_meta])
+        if (problema.es_objetivo(estado_actual)):
+            break
+
+        acciones_validas = problema.generar_acciones(estado_actual)
+        print("Acciones válidas desde el estado actual:", [
+              accion.nombre for accion in acciones_validas])
+
+        accion = mando.tomar_decision(acciones_validas)
+        print("El agente decidió: {}".format(str(accion.get_nombre())))
+
+        nuevo_estado = problema.resultado(estado_actual, accion)
+        mando.set_coordenadas(nuevo_estado.get_coordenadas())
+        print("Las coordenadas del agente son: {}".format(
+            str(mando.get_coordenadas())))
+
+        estado_actual = nuevo_estado
+        nuevo_estado = None

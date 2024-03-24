d65718ff2a98098a3f2670bc143a77068387faec
Juan David <juan.loaiza.santiago@correounivalle.edu.co>
2024-03-24 08:48:58 -0500

    window initial position improvement

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 # [main.py]
 
-from controllers.Controlador_juego import Controlador_juego as controlador
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from controllers.Controlador_juego import Controlador_juego as Controlador
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDesktopWidget
 import sys
+
+
+def center(main_window: QMainWindow):
+    qr = main_window.frameGeometry()
+    cp = QDesktopWidget().availableGeometry().center()
+    qr.moveCenter(cp)
+    main_window.move(qr.topLeft())
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     main_window = QMainWindow()
-    controlador = controlador()
+    center(main_window)
+    controlador = Controlador()
     controlador.mostrar(main_window)
     sys.exit(app.exec_())

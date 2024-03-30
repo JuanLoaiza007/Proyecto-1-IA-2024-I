56fde7a27a795c9e56dcb18c1e32d80407f34141
Juan David <juan.loaiza.santiago@correounivalle.edu.co>
2024-03-30 09:46:56 -0500

    deleted center MainWindow

--- main.py
+++ main.py
@@ -5,17 +5,9 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QDesktopWidget
 import sys
 
 
-def center(main_window: QMainWindow):
-    qr = main_window.frameGeometry()
-    cp = QDesktopWidget().availableGeometry().center()
-    qr.moveCenter(cp)
-    main_window.move(qr.topLeft())
-
-
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     main_window = QMainWindow()
-    center(main_window)
     controlador = Controlador()
     controlador.mostrar(main_window)
     sys.exit(app.exec_())

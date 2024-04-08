f5baf2d3d9b08259699bc36c66ea28b7e83e1a1c
Sebastian Muñoz <juan.munoz.rojas@correounivalle.edu.co>
2024-04-08 14:03:15 -0500

    avances (tiene errores)

--- models/trees/Costo_uniforme.py
+++ models/trees/Costo_uniforme.py
@@ -15,4 +15,37 @@ class Costo_uniforme:
         cola_prioridad.put(Nodo.get_costo_acumulado, Nodo(problema))
         
         while not cola_prioridad.empty():
-            nodo: Nodo = cola_prioridad.get()  # Saca un nodo de la cola        
+            nodo: Nodo = cola_prioridad.get()  # Saca un nodo de la cola  
+            
+            if nodo.es_meta():
+                camino = Costo_uniforme.reconstruir_camino(nodo)
+                reporte = Costo_uniforme.generar_reporte(
+                    nodos_expandidos, profundidad, tiempo_inicio, nodo)
+
+                return camino, 'Encontré a grogu', reporte 
+            
+            # Expande el nodo y obtiene sus hijos
+            hijos: "list[Nodo]" = nodo.expandir()
+
+            # Actualiza cantidad de nodos expandidos
+            nodos_expandidos += 1      
+            
+            cola_prioridad.get()            
+            
+            for hijo in hijos:
+
+                # Actualiza profundidad
+                if profundidad < hijo.get_profundidad():
+                    profundidad = hijo.get_profundidad()
+
+                if evitar_devolverse and Costo_uniforme.se_ha_devuelto(hijo):
+                    print_debug("paso, queria ir a {} pero hace dos pasos estuve en {}".format(
+                        str(hijo.get_estado()), str(nodo.get_padre().get_estado())))
+                    continue
+
+                if evitar_ciclo and Costo_uniforme.cayo_en_ciclo(hijo):
+                    print_debug("paso, hace un tiempo estuve en {}, evitare entrar en ciclo".format(
+                        str(hijo.get_estado())))
+                    continue
+                                
+                cola_prioridad.put(hijo)  # Agrega los hijos a la cola                       
